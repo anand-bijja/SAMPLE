56701d0833aefec9a2c62460f31b10afc1bddd9e
anand-bijja <100574847+anand-bijja@users.noreply.github.com>
2022-02-28 17:38:49 +0530

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace ConsoleApp14
+{
+   
+ 
+    class Program
+    {
+        static bool keepGoing;
+        static void Main(string[] args)
+        {
+            keepGoing = true;
+            Worker worker = new Worker(new KeepGoingDelegate(KeepGoing));
+            Thread thread = new Thread(worker.DoWork);
+            thread.IsBackground = true;
+            thread.Start();
+
+            while (thread.ThreadState != ThreadState.Stopped)
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'p':
+                        keepGoing = false;
+                        break;
+                    case 'w':
+                        keepGoing = true;
+                        break;
+                    case 's':
+                        worker.Stop();
+                        break;
+                }
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        static bool KeepGoing()
+        {
+            return keepGoing;
+        }
+    }
+
+    public delegate bool KeepGoingDelegate();
+    public class Worker
+    {
+        bool stop = false;
+        KeepGoingDelegate KeepGoingCallback;
+        public Worker(KeepGoingDelegate callbackArg)
+        {
+            KeepGoingCallback = callbackArg;
+        }
+
+        public void DoWork()
+        {
+            while (!stop)
+            {
+                Console.Write(KeepGoingCallback() ? "\rWorking" : "\rPaused ");
+
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("\nStopped");
+        }
+
+        public void Stop()
+        {
+            stop = true;
+        }
+    }
+}
+    
